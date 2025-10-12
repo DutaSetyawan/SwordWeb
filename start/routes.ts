5ad6ae680e06348801c1a895a7ebae3b4c9a8db5
Duta Setyawan <dutasetyawan1203@gmail.com>
2025-10-12 13:25:02 +0700

    Initial commit

---
+++ start/routes.ts
@@ -0,0 +1,14 @@
+/*
+|--------------------------------------------------------------------------
+| Routes file
+|--------------------------------------------------------------------------
+|
+| The routes file is used for defining the HTTP routes.
+|
+*/
+
+import router from '@adonisjs/core/services/router'
+
+router.on('/').render('pages/index')
+router.on('/products').render('pages/products')
+router.on('/contact').render('pages/contact')
